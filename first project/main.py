d67cc6149597b7559d2f7d18e306d34970838d5e
Chronin7 <liam.perl@ucas-edu.net>
2024-09-06 10:17:12 -0600

    fineshd game

--- first project/main.py
+++ first project/main.py
@@ -1,35 +1,35 @@
 import random
-playagenn = 0
-num = random.randint (1,100)
-y = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60}
-print("Welcome the GUESS THE NUMBER! you have 20 atempts before you lose the game. good luck.")
-guess = int(input("Guess a number 1-100: "))  
-x = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
-for x in range(20): 
-  if guess < num :
-    guess = int(input ("the number is larger: "))
-  if guess > num :
-    guess = int(input ("the number is smaller: "))
-  if guess == num : 
-    print ("you got it")
-    playagenn = int(input ("play agen 1=yes 2=no: "))
-    break
-if  playagenn == 2:
-  quit
-elif playagenn == 1:
-  playagenn = 0
-  num = random.randint (1,100)
 
-  print("Welcome the GUESS THE NUMBER! you have 20 atempts before you lose the game. good luck.")
-  guess = int(input("Guess a number 1-100: "))  
-  x = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
-  for x in range(20): 
-    if guess < num :
-      guess = int(input ("the number is larger: "))
-    if guess > num :
-      guess = int(input ("the number is smaller: "))
-    if guess == num : 
-      print ("you got it")
-    if x == 20:
-      print("Game Over")
-      quit
+easterEggCount = 100+1
+maxGuessCount = 20
+
+minGuess = 1
+maxGuess = 100
+
+
+playCount = 0
+playAgain = True
+while playAgain:
+	playCount += 1
+
+	if playCount == easterEggCount:
+		print("WHY DID YOU WASTE YOUR TIME ON THIS DUMB GAME! DO SOMETHING BETIER WITH YOUR TIME!")
+		exit()
+
+
+	num = random.randint (minGuess,maxGuess)
+	print(f'Welcome the GUESS THE NUMBER! you have {maxGuessCount} attempts before you lose the game. good luck.')
+	guess = int(input(f'Guess a number {minGuess}-{maxGuess}: '))  
+
+	for x in range(maxGuessCount): 
+		if guess < num :
+			guess = int(input ("the number is larger: "))
+		if guess > num :
+			guess = int(input ("the number is smaller: "))
+		if guess == num : 
+			print ("you got it")
+			playAgain = input ("play again? (y/n): ")
+			if playAgain != "y":
+				print("ok")
+				playAgain = False
+			break
