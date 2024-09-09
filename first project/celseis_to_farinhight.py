bf5ac50aaee36d9079ac77b55c26d6b8e61132ae
Chronin7 <liam.perl@ucas-edu.net>
2024-09-09 10:22:55 -0600

    Update celseis_to_farinhight.py

--- first project/celseis_to_farinhight.py
+++ first project/celseis_to_farinhight.py
@@ -2,11 +2,11 @@
 yes = input("woud you like celsius to fahrenheit (y/n): ")
 if yes == "y":
     tempofcelsius = int(input("what is the temp in celsius: ")) 
-    print ("the temp for fahrenheit is {tempofcelsius*(9/5)+32}")
+    print ("the temp for fahrenheit when celsius is",tempofcelsius,"is",tempofcelsius*(9/5)+32)
 if yes == "n":
     no = input("woud you like celsius to fahrenheit (y/n): ")
     if no == "y":
         tempforfahrenheit = int(input("what is the temp in fahrenheit: ")) 
-        print("the temp for celsius is {tempforfahrenheit-32*(5/9)}")
+        print("the temp for celsius when fahrenheight is ",tempforfahrenheit,"is",tempforfahrenheit-32*(5/9))
 
 quit
