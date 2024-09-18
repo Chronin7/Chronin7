599d5b774f5f1d6bd460f178fdc6cb3a49c08b3d
Chronin7 <liam.perl@ucas-edu.net>
2024-09-18 10:15:50 -0600

    new servay

--- info_converter.py
+++ info_converter.py
@@ -1,7 +1,11 @@
 #ProficiencyTest: Personal Information Converter liam perl
 name = str(input("what is your name: "))
-age = int("what is your age (in numbers pleese): ")
+age = int(input("what is your age (in numbers pleese): "))
 hight_in_meeters = int(input("what is your hight in meeters(number pleese): "))
-favcoller = str(input("what is your favrite coller: "))
+favnum = str(input("what is your favrite number: "))
 
-print(f"name:{name} age:{age} hight in meters:{hight_in_meeters} favorit coller")
+print(f"name:{name} age:{age} hight in meters:{hight_in_meeters} favorit number: {favnum}")
+agetwo = str(age)
+hight_in_meeterstwo = float(hight_in_meeters)
+favenumtwo = str(favnum)
+print(f"name:{name} age: {agetwo} hight in meters: {hight_in_meeterstwo} favorit number: {favenumtwo}")
