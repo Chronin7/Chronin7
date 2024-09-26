641527b2fa47de68d0a52a22d0d4ce982620a416
Chronin7 <liam.perl@ucas-edu.net>
2024-09-26 10:48:41 -0600

    new seacret mesageing sistom

--- first project/seacret_code.py
+++ first project/seacret_code.py
@@ -1,10 +1,27 @@
 code_decode = int(input("would you like to code (1) or decode (2): "))
-coded_list = []
-coded = ""
+list_o_coded = []
+output =""
+decoded = ""
+iteration = 1
+loops_o_codeing = 0
+list_o_decoding = []
 if code_decode == 1:
     code = input("what is the uncoded word: ")
-    for i in code:
-        coded_list.append(ord(i))
-    for x in coded_list:
-        coded = coded + str(x)
-print(coded)
+    seed = int(input("what is the decoding seed (whole numbers pleese): "))
+    for x in code:
+        list_o_coded.append(((ord(x))))
+    for y in list_o_coded:
+        if loops_o_codeing == 0:
+            output = str(int(y)+seed)
+        else:
+            output = output +","+ str(int(y)+seed)
+        loops_o_codeing += 1
+    print(output)
+if code_decode == 2:
+    coded_decoder = str(input("what is the coded thing: "))
+    seed_o_decodeing = int(input("what is the seed: "))
+    list_o_decoding = (coded_decoder.split(","))
+    for z in list_o_decoding:
+        var = str(chr(int(z)-seed_o_decodeing))
+        decoded = decoded + var
+    print(decoded)
