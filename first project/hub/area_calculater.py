cce4266323f0245e6129ae5ec4f450bc0798d0bd
Chronin7 <liam.perl@ucas-edu.net>
2024-10-04 10:47:07 -0600

    stuff

--- first project/hub/area_calculater.py
+++ first project/hub/area_calculater.py
@@ -9,10 +9,11 @@ def type_text(text):
 type_text("Hi i am Arion. I am a area finder")
 while True:
 	e = 1
-	type_text("1 for square/rectangle")
-	type_text("2 for triangle")
-	type_text("3 for circle")
-	type_text("4 for trapezoid")
+	type_text("1 for rectangle")
+	type_text("2 for square")
+	type_text("3 for triangle")
+	type_text("4 for circle")
+	type_text("5 for trapezoid")
 	shape = int(input("what do you want or type stop to stop: "))
 	if shape == "1":
 		#square/rectangle
@@ -21,20 +22,20 @@ while True:
 		b = int(input("what is the length (only numbers please): "))
 		type_text(f"when the hight is {a} and the length is {b} the area is {a*b}.")
 		e = 0
-	if shape == "3":
+	if shape == "4":
 		#circle
 		type_text("ok input the numbers that you want")
 		e = 0
 		a = int(input("what is the hight (only numbers please): "))
 		type_text(f"when the radius is {a} the area is {3.141592653589793238462643383279*a^2}")
-	if shape == "2":
+	if shape == "3":
 		#triangle
 		type_text("ok input the numbers that you want")
 		e = 0
 		a = int(input("what is the hight (only numbers please): "))
 		b = int(input("what is the length (only numbers please): "))
 		type_text(f"when the hight is {a} and the length is {b} the area is {(a*b)*(1/2)}.")
-	if shape == "4":
+	if shape == "5":
 		#trapezoid
 		type_text("ok input the numbers that you want")
 		e = 0
@@ -42,8 +43,11 @@ while True:
 		b = int(input("what is the top (only numbers please): "))
 		c = int(input("what is the bottom (only numbers please): "))
 		type_text(f"when the hight is {a} and the bottom is {b} and the top is {c} the area is {((b+c)/2)*a}.")
-	if shape == "5":
-		type_text("sorry coming soon")
+	if shape == "2":
+		type_text("ok input the numbers that you want")
+		e = 0
+		a = int(input("what is the length of one of the sides (only numbers please): "))
+		type_text(f"when the hight is {a} and the length is {b} the area is {a*2}.")
 		e = 0
 	if shape == "6":
 		type_text("sorry coming soon")
--- first project/hub/pig.py
+++ first project/hub/pig.py
@@ -1,68 +1,87 @@
 import random
 import time
+#this function is from geeks for geeks
+def check_space(stringer):
+    count = 0
+    for i in range(0, len(stringer)):
+        if stringer[i] == " ":
+            count += 1
+         
+    return count
 def type_text(text):
 	for x in text:
 		print(x, end = "", flush = True)
 		time.sleep(random.uniform(.01,1.0))
 	print("")
-
+out = []
+outer_iteration = 1
+oouutt = ""
 while True:
-	input_word = input("I am Pig what do you want to translate (one word at a time):")
-	if input_word == "":
+	inp = input("I am Pig what do you want to translate:")
+	if inp == "":
 		print("oops looks like you are a bit trigerhappy")
 	else:
-		checks = 0
-		split = []
-		deleted = ""
-		not_a_string = ""
-		output = ""
-		not_a = ""
-		while True:
-			iteration = 1
-			if len(input_word) < 3:
-				print(input_word)
-				
-			else:
-				for x in input_word:
-					
-					if x == "a":
-						split = input_word.split("a",1)
-						deleted = "a"
-						
-						break
-					if x == "e":
-						split = input_word.split("e",1)
-						deleted = "e"
-						
-						break
-					if x == "i":
-						split = input_word.split("i",1)
-						deleted = "i"
-						
-						break
-					if x == "o":
-						split = input_word.split("o",1)
-						deleted = "o"
-						
-						break
-					if x == "u":
-						split = input_word.split("u",1)
-						deleted = "u"
-						
-						break
-					if x == "y" and iteration > 1:
-						split = input_word.split("y",1)
-						deleted = "y"
-						
-						break
-					iteration += 1
-			
-			not_a_string = split[:iteration]
-			for i in not_a_string:
-				not_a = not_a +''+i
-			output = not_a + deleted +"ay"
-			print(output)
-			go = input("Do you want to translate another word? (y/n): ")
+		imp = inp.split(" ")
+		for i in imp:
+			input_word = i
+			checks = 0
+			splitt = []
+			deleted = ""
+			not_a_string = ""
+			output = ""
+			not_a = ""
+			while True:
+				iteration = 1
+				if len(input_word) < 3:
+					if outer_iteration == 1:
+						out.append(input_word)
+					else:
+						out.append(" ")
+						out.append(input_word)
+				else:
+					for x in input_word:
+						if x == "a":
+							splitt = input_word.split("a",1)
+							deleted = "a"
+							break
+						if x == "e":
+							splitt = input_word.split("e",1)
+							deleted = "e"
+							break
+						if x == "i":
+							splitt = input_word.split("i",1)
+							deleted = "i"
+							break
+						if x == "o":
+							splitt = input_word.split("o",1)
+							deleted = "o"
+							break
+						if x == "u":
+							splitt = input_word.split("u",1)
+							deleted = "u"
+							break
+						if x == "y" and iteration > 1:
+							splitt = input_word.split("y",1)
+							deleted = "y"
+							break
+						iteration += 1
+				not_a_string = splitt[:iteration]
+				for i in not_a_string:
+					not_a = not_a +''+i
+				output = not_a + deleted +"ay"
+				if outer_iteration == 1:
+					out.append(input_word)
+				else:
+					out.append(" ")
+					out.append(input_word)
+				if len(out) == len(imp):
+					break
+				else:
+					outer_iteration += 1
+			for z in out:
+				oouutt = oouutt + z
+			print(oouutt)
+			go = input("Do you want to translate again? (y/n): ")
 			if input_word == "":
 				print ("oops looks like you are a bit trigerhappy")
 			else:
