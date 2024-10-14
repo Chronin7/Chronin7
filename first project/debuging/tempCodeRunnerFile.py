7ac0f40f670bbaf146bd311b28b213d3f8bb49b6
Chronin7 <liam.perl@ucas-edu.net>
2024-10-14 10:51:52 -0600

    ccccccvfrbbvcvcvcvfvf

---
+++ first project/debuging/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+# an integer with the comma to separate the thousands
+# a float with 4 decimal places
+# a percentage
+# and then a dollar amount
--- first project/debuging/test.py
+++ first project/debuging/test.py
@@ -1,37 +1,32 @@
-# an integer with the comma to separate the thousands
-# a float with 4 decimal places
-# a percentage
-# and then a dollar amount
+q1111111111111111 = "      ███████████      "
+q2222222222222222 = "    ███         ███    "
+q3333333333333333 = "   ███           ███   "
+q4444444444444444 = "   ███           ███   "
+q5555555555555555 = "   ███           ███    "
+q6666666666666666 = ""
+q7777777777777777 = ""
+q8888888888888888 = ""
 
-uot = ""
-inp = input("imput numbers to chace there format:")
-le = len(inp)
-lis = []
-out = []
-it = 0
-itt = -1
-outt = []
-first = inp[1]
-for x in inp:
-	outt.append(x)
-while le-1 != it:
-	it += 1
-	lis.append(inp[it])
-for r in lis:
-	itt += 1
-	if itt % 3 == 0:
-		out.append("""'""")
-		out.append(r)
-	else:
-		out.append(r)
-for x in out:
-	first += x
-print(first)
-outt [:4] = str(".")+str(outt[:4])
-u = ""
-for x in outt:
-	u += x
-print(x)
-uot [:2] = str(".")+str(outt[:2])
-print(f"%{inp}")
-print(f"${uot}")
+q1 = f"""
+	                     ██
+   {q1111111111111111}   ██
+   {q2222222222222222}   ██
+   {q3333333333333333}   ██
+   {q4444444444444444}   ██
+   {q5555555555555555}   ██
+   {q6666666666666666}   ██
+   {q7777777777777777}   ██
+   {q8888888888888888}   ██
+						 ██
+"""
+print(q1)
+
+
+       ███████████
+    ███           ███
+   ███             ███
+   ███             ███
+   ███             ███
+   ███             ███
+	███           ███
+	   ███████████
