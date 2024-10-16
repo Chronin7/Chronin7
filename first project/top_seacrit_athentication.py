b19e2c3166dc414dfccc7abb2b6d5dfde865535b
Chronin7 <liam.perl@ucas-edu.net>
2024-10-16 10:35:14 -0600

    f

--- first project/top_seacrit_athentication.py
+++ first project/top_seacrit_athentication.py
@@ -1,6 +1,10 @@
 username = input("what is your username: ")
 admin = ["liam","ms larose", "jonas","bob","tom","tim","mat","joe"]
+user = ["addy","maddy","daddy","mom","peat",]
 if username not in admin:
-	quit
+	if username not in user:
+		quit
+	else:
+		print("you do not have high enugh clerace")
 else:
 	print("in")
