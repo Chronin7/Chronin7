f000f8375447755da1ede01753dd3b2929befeea
Chronin7 <liam.perl@ucas-edu.net>
2024-10-22 10:52:58 -0600

    fa

--- first project/debuging/fe.py
+++ first project/debuging/fe.py
@@ -1,12 +1,5 @@
-# an integer with the comma to separate the thousands
-# a float with 4 decimal places
-# a percentage
-# and then a dollar amount
-format
-inp = int(input("imput numbers to chace there format:"))
-pr = "{:%}"
-print(pr.format(0.54))
-pr ="{:,}"
-print(pr.format(inp))
-pr =str(inp)[:4], str(".")+str(str(inp)[:4])
-print(pr[1])
+f = input()
+out = []
+for x in f:
+	out.append(f""" "{x}" """)
+print(out)
