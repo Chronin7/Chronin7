9d8854284de5c2f5bee4ca4e2c8d8213a87ce4f0
Chronin7 <liam.perl@ucas-edu.net>
2024-10-24 10:17:05 -0600

    e

--- first project/debuging/bug2.py
+++ first project/debuging/bug2.py
@@ -1,21 +1,20 @@
+
 # This function should return the sum of all even numbers in the list, but it returns the wrong value. Find the bug.
+# you returned if the number was not even wich stops the function
+# logic error
 def sum_even_numbers(numbers):
 
-	 sum = 0
+     summ = 0
 
-	 for num in numbers:
+     for num in numbers:
 
-		  if num % 2 == 0:
+          if num % 2 == 0:
 
-			   sum += num
+               summ += num
 
-		  else:
+          else:
 
-			   return 0
+               continue
 
-	 return sum
-print(sum_even_numbers(int(input(":"))))
-
-in
-for x in inpit:
-	 print x
+     return summ
+print(sum_even_numbers([1,2,3,4,5,6,7,8,9,10]))
