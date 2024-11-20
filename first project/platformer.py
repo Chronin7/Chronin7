4fefe3ba3e244f8b762bc91e26c231966272fb14
chronin7 <liam.perl@ucas-edu.net>
2024-11-20 10:28:04 -0700

    I FIXED MY HUB YAY

--- first project/platformer.py
+++ first project/platformer.py
@@ -10,30 +10,24 @@ import msvcrt
 def main():
 		global xcord
 		global ycord
-			key = ""
-			if msvcrt.kbhit():
-				key =  inputit()
-			if key == "w"and ycord !=0: 
-				ycord-=1
-			elif key == "a":
-				xcord-=1
-			elif key == "s":
-				ycord+=1
-			elif key == "d":
-				xcord+=1
-			elif key == "p":
-				quit()
-			print(" ")
-			for x in range(ycord-1):
-				print("                                                  ")
-			for x in range(xcord):
-				print(" ",end="")
-			print("x",end="")
-			threading.Thread(target=inputit).start
-def inputit():
-	if msvcrt.kbhit():
-		return msvcrt.getch().decode("utf-8").lower()
-	else:
-		return
-def tic():
-	main()
+		key = ""
+		if msvcrt.kbhit():
+			key =  inputit()
+		for x in range(500):
+			print()
+		if key == "w"and ycord !=0: 
+			ycord-=1
+		elif key == "a":
+			xcord-=1
+		elif key == "s":
+			ycord+=1
+		elif key == "d":
+			xcord+=1
+		elif key == "p":
+			quit()
+		print(" ")
+		for x in range(ycord-1):
+			print("                                                  ")
+		for x in range(xcord):
+			print(" ",end="")
+		print("x",end="")
