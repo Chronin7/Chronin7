7b01a8dc3747f3808275ccfad8a11a6de0bb3ba8
chronin7 <liam.perl@ucas-edu.net>
2024-11-25 10:51:19 -0700

    CODING ASCII SUCKS

--- first project/hub/final.py/final_mon.py
+++ first project/hub/final.py/final_mon.py
@@ -1,7 +1,14 @@
 import random
 import time
-def sumon(monhp,monname,damage,descrion,montype):
-	print(f"name: {monname}")
-	print(f"{montype} health: {monhp}")
-	print(descrion)
-	
+from asccii_art import *
+def sumon(monhp,monname,descrion,montype):
+	ascii_art(f"name: {monname}")
+	ascii_art(f"health: {monhp}")
+	ascii_art("descrion:")
+	ascii_art(descrion)
+def mon_damage(mon_name,diedamage):
+	damage = random.randint(1,diedamage)
+	ascii_art(f"{mon_name} dealt {damage}@damage")
+	return damage
+mon_damage("bob",20)
+sumon("100","killer","will kill you","human")
