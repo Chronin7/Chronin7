c0d98ed8673bb0b5b3ce8a1060433ce07993ba6d
chronin7 <liam.perl@ucas-edu.net>
2025-08-15 09:46:15 -0600

    calculater ++ coming soon

--- first project/debuging/test.py
+++ first project/debuging/test.py
@@ -1,23 +1,36 @@
-inp=str(input())
-data=[]
-placeholder=""
-for x in inp:
-	if x == "-" and placeholder != "-":
-		placeholder=x
-	elif placeholder == "-":
-		data.append("-"+str(x))
+
+equating = True
+while equating == True:
+	inp=str(input())
+	data=[]
+	try:
 		placeholder=""
-	else:
-		data.append(x)
-try:
-	data[0]-1
-except:
-	print("error")
-print(data)
-operaters=["!","+","/","%","^","(",")","*"]
-typeof=[]
-for x in data:
-	if x in operaters:#add the iteration count
-		for y in operaters:
-			if x==y:
-				typeof.append(operaters[iteration])
+		for x in inp:
+			if x == "-" and placeholder != "-":
+				placeholder=x
+			elif placeholder == "-":
+				data.append(int("-"+str(x)))
+				placeholder=""
+			else:
+				data.append(int(x))
+			print(data)
+		operaters=["+","/","%","^","(",")","*"]
+		typeof=[]
+		for x in data:
+			if x in operaters:
+				iteration=-1
+				for y in operaters:
+					iteration+=1
+					if x==y:
+						typeof.append(operaters[iteration])
+			elif "-" in x:
+				typeof.append("negitive")
+			else:
+				typeof.append("number")
+		print(data)
+		print(typeof)
+		iteration = -1
+		for i in typeof:#figure out how to add the operations
+			if i == "!"
+	except:
+		print("please input an equason (no ='s or ()'s (keep in mind i am not smart enugh to do order of operations so make shure that it is in the corect order (sorry)) ")
