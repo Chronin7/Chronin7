37bbd9b7b3b4a77a741d1f64bf42ad2d4f301064
Chronin7 <liam.perl@ucas-edu.net>
2025-08-22 09:43:11 -0600

    fa

--- year 2/who_are_you_y2.py
+++ year 2/who_are_you_y2.py
@@ -1,3 +1,4 @@
+#liam p who are you
 namelist=[]
 agelist=[]
 collorlist=[]
@@ -5,11 +6,11 @@ while True:
 	input_name=input("whats your name: ")
 	if input_name in namelist:
 		pos=namelist.index(input_name)
-		print(f"hello {input_name} your age is {agelist[pos]} and your favorit collor is {collorlist[pos]}")
+		print(f"hello {input_name} your age is {agelist[pos]} and your favorit collor is {collorlist[pos]}.")
 	else:
 		age=input("what is your age: ")
 		collor=input("whats your favrit coller: ")
 		namelist.append(input_name)
 		agelist.append(age)
 		collorlist.append(collor)
-		print(f"hello {input_name} your age is {age} and your favorit collor is {collor}")
+		print(f"hello {input_name} your age is {age} and your favorit collor is {collor}.")
