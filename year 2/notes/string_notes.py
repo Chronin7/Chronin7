0281d409e32b065a9842498751b7f854ba94eccc
Chronin7 <liam.perl@ucas-edu.net>
2025-09-04 09:14:37 -0600

    e

---
+++ year 2/notes/string_notes.py
@@ -0,0 +1,12 @@
+#What is a method?
+#it is a way to alter a string in a predefined way
+#How do I write a method?
+#string.upper() would make it all uppercase
+#What methods can I use to change the formatting of my string?
+#f strings are a way to input variables into a string
+#What methods allow me to search the contents of my string?
+# find() will return the location of whatever you are looking for but will return a -1 if none is found
+#What methods allow me to change the contents of my string?
+# string_after=string_before.method()
+#Why is it important to know what type of output a method will give me?
+# so you dont get a unexpected value
