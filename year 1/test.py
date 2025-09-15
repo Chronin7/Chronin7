3f6eb964bae0e20443b03daaea1b017a4c4ca0ec
Chronin7 <liam.perl@ucas-edu.net>
2025-09-15 09:54:52 -0600

    i have found a way (hopefully)

--- year 1/test.py
+++ year 1/test.py
@@ -1,13 +1,28 @@
-import curses
+import threading
+import time
 
-def get_single_char():
-	screen = curses.initscr()
-	curses.noecho()
-	curses.cbreak()
-	char = screen.getch()
-	curses.endwin()
-	return chr(char)
+user_input = None
 
-print("Press any key:")
-key = get_single_char()
-print(f"You pressed: {key}")
+def get_input():
+	global user_input
+	user_input = input("Enter something: ")
+
+# Start input in a separate thread
+input_thread = threading.Thread(target=get_input)
+
+input_thread.daemon = True # Allow main program to exit even if thread is running
+while True:
+	try:
+		input_thread.start()
+	except:
+		pass
+	# Main program continues execution
+	print("Doing other work...")
+	time.sleep(.1)
+	if user_input == "e":
+		print("var e added")
+	else:
+		print("none")
+
+	print("More work...")
+	user_input=""
