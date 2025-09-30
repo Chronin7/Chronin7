a29e4287b0f401e35099eeafa4fc5b4b06da46ee
Chronin7 <liam.perl@ucas-edu.net>
2025-09-30 09:52:44 -0600

    SOUND UNLOCKED

---
+++ year 2/text_to_speach.py
@@ -0,0 +1,25 @@
+import pyttsx3, time
+
+def text_init():
+	"""initeates the text to speach reader"""
+	global engine
+	engine = pyttsx3.init()
+def set_volume(sound):
+	"""sound needs to be a float 0.0-1.0"""
+	global engine
+	engine.setProperty('volume',sound)
+	text_init()
+def set_speed(speed):
+	global engine
+	text_init()
+	engine.setProperty('rate',speed)
+def add_to_que(text):
+    engine.say(text)
+def speak():
+    """speaks all text in the que"""
+    global engine
+    engine.runAndWait()
+    engine.stop()
+text_init()
+add_to_que("hi this is faster then fast dinosaur")
+speak()
