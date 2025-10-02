ec326f7d8dbf76b1149c8114e0373ce016aeb043
Chronin7 <liam.perl@ucas-edu.net>
2025-10-02 08:49:15 -0600

    yiee

--- year 1/first project/debuging/timetable.py
+++ year 1/first project/debuging/timetable.py
@@ -1,4 +1,6 @@
 # LP 1 times table
-num = int(input("what times tables do you want: "))
+
 for x in [1,2,3,4,5,6,7,8,9,10,11,12]:
-	print(num*x)
+	for y in [1,2,3,4,5,6,7,8,9,10,11,12]:
+		print(x*y,end=" ")
+	print()
