19c72af7aa4aa3aae44db665c63bffd0ad5ac67d
Chronin7 <liam.perl@ucas-edu.net>
2025-10-06 09:31:52 -0600

    NI

--- year 1/first project/tempCodeRunnerFile.py
+++ year 1/first project/tempCodeRunnerFile.py
@@ -1,10 +1,1 @@
-
-imp = int(input())
-coma = "{:,}"
-print(coma.format(imp))
-per = "{:%}"
-print(per.format(imp))
-per = "{:3f}"
-print(per.format(imp))
-per = "${:2d}"
-print(per.format(imp))
+}/
