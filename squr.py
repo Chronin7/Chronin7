5e53c01c89e47353dc89d869f846bc20b539197f
Chronin7 <liam.perl@ucas-edu.net>
2025-11-20 09:51:12 -0700

    va

--- squr.py
+++ squr.py
@@ -1,3 +1,11 @@
-import util_functions
-nums=[]
-while (bool(choise:=util_functions.get_valid_type(int,"input 1 number to square (0 to get results): "))):([print(f"{nums[index]}^2={x}") for index,x in enumerate(map(lambda num: num*num,nums))]) if choise else nums.append(choise)
+exec('import util_functions as u;l=[];v=1\nwhile v==1:c=u.get_valid_type(int,"");l.append(c)if bool(c) else([print(f"{l[i]}:{x}")for i,x in enumerate(map(lambda n:n*n,l))]);v=1 if bool(c)else 0')
+#set nums to empty list
+#set var to 1
+#while var is 1
+# set choice to user input
+# if choice is 0
+#  for index,x in nums
+#	output index of nums and lamda num:num*num,nums
+#	set var to 0
+# otherwise
+#  add choice to nums
