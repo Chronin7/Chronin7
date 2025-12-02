df6130166cd90aae5659aafc45643daba835f461
Allen Perl <allen.perl@narvar.com>
2025-12-02 16:49:09 -0700

    kk

--- final_y2.py
+++ final_y2.py
@@ -49,19 +49,42 @@ class inventory:
 			self.invin[item][0]+=count
 		else:
 			self.invin[item]=[1,effect,description]
+		print(f"you got {item}")
 	def use(self,item,person=None):
 		if self.inven[item]==1:
-			effect(self.invin[item][1])
+			effect(self.invin[item][1],person)
 		del self.invin[item]
 def effect(efec,person):
+	global hp
+	global party
 	if 'heal' in efec:
-		hp+=[1]
+		person["hp"]+=efec[1]
 	elif "heal+" in efec:
-		hp=max_hp
-	elif "mage bomb" in efec:
-		damage(efec[1],efec[2],person)
-	elif 1==1:
-		print()
+		person["hp"]+=efec[1]
+	elif "heal++" in efec:
+		person["hp"]=person["max_hp"]
+	elif "heal+++" in efec:
+		for i in len(party):
+			party[i]["hp"]=party[i]["max_hp"]
+	elif "eather" in efec:
+		person["mana"]+=efec[1]
+	elif "elexer" in efec:
+		person["mana"]=round(person["max_mana"]/2)
+		person["hp"]=round(person["max_hp"]/2)
+	elif "elexer+" in efec:
+		person["mana"]=person["max_mana"]
+		person["hp"]=person["max_hp"]
+	elif "bomb core" in efec:
+		damage(efec[1],"fire","e1")
+		damage(efec[1],"fire","e2")
+		damage(efec[1],"fire","e3")
+	elif "frost core" in efec:
+		damage(efec[1],"ice",person)
+	elif "lightning core" in efec:
+		damage(efec[1],"lightning","e1")
+		damage(efec[1],"lightning","e2")
+		damage(efec[1],"lightning","e3")
+
 class enemy:
 	def __init__(self,name,dresestance,damage,hp,tier,descrip="this shuldent be printed but if it is be proud of yourself, you found a glitch (unless you are the maker, then have an emotional brakedown)"):
 		self.name=name
@@ -89,10 +112,11 @@ class enemy:
 			self.dead()
 
 	def dead(self):
-		print("dead")
+		print(f"dead {self.name}") 
 		del self
 	
-def damage(damage,dtype,person,person_list):
+def damage(damage,dtype,person):
+	global person_list
 	if person=="e1":
 		person_list[0].damage(damage,dtype)
 	elif person=="e2":
@@ -105,18 +129,59 @@ def damage(damage,dtype,person,person_list):
 		person_list[4]-=damage
 	elif person=="p3":
 		person_list[5]-=damage
-def proses_pos(type,pos,map,speshal=None,item=None,gtype="f",iitem="filler",efect="",descrip="this shuldent be printed but if it is be proud of yourself, you found a glitch (unless you are the maker, then have an emotional brakedown)"):
+def proses_pos(gtype,pos,speshal=None,item=None,iitem="filler",efect="",descrip="this shuldent be printed but if it is be proud of yourself, you found a glitch (unless you are the maker, then have an emotional brakedown)"):
 	if gtype == "w":
 		print("how did you get here")
-		Location_type = "illegal"
+		location_type = "illegal"
 	elif gtype == "l": 
 		location_type = "illegal"
 	elif gtype == "i":
 		location_type = "item_type"
 		inventory.grant(inventory,iitem,efect,1,descrip)
-	elif gtype==1:
-		print()
+	elif gtype=="h":
+		location_type="hot_ground"
+	elif gtype=="speshal":
+		location_type="speshal"
+	elif gtype=="ni":
+		location_type="nesecary_item"
+	elif gtype=="s":
+		location_type="skylift"
+	elif gtype=="c":
+		location_type="cave"
+	elif gtype=="b":
+		location_type="bridge"
+	elif gtype=="u":
+		print("how did you get here")
+		location_type="illegal"
+	elif gtype=="f":
+		location_type="forest"
+	elif gtype=="t":
+		location_type="town"
+	elif gtype=="p":
+		location_type="plain"
+	elif gtype=="r":
+		location_type="river"
+	elif gtype=="spawn":
+		location_type="spawn_point"
+	elif gtype=="s":
+		location_type="snow"
+	elif gtype=="aw":
+		location_type="acssessible_water"
+	return {"location_type":location_type,
+			"position":pos,
+			"speshal":speshal,
+			"item":item},{"iitem":iitem,
+			"efect":efect,
+			"descrip":descrip
+			}
 
 	
 if __name__ == "__main__":
-    print("yes")
+	inventory=inventory({})
+	pos=[13,20]
+	current_location,item=proses_pos(map[pos[0]][pos[1]],pos)
+	print(current_location)
+	print(item)
+	print(inventory.invin)
+	print(len(inventory))
+	inventory.grant("potion",["heal",50],1,"a small red potion that heals 50 hp")
